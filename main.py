f738928fe79d0a6f12995c4f489be98edf2a549d
digidecor3d <45209978+digidecor3d@users.noreply.github.com>
2022-01-23 01:43:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print("gittest3")
+    print("gittest333333")
 
 
 # Press the green button in the gutter to run the script.
